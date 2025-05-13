3b30c77d71ef3835bf712872dc8345f8dd3e010d
Rifaldi Ali <figmapaulo@gmail.com>
2025-05-13 18:40:36 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import TwitGenerator from "./components/TwitGenerator";
-import Head from 'next/head';
+
 
 export default function Home() {
   return (
-    <div className="relative min-h-[100vh] h-full flex justify-center items-center bg-black">
+    <div className="min-h-screen max-w-[460px] w-full mx-auto flex items-center justify-center bg-black">
       <TwitGenerator />
     </div>
   );
